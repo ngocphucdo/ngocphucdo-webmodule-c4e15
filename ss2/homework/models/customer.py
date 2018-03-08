8d111788db9d64efd323591df6488e8f39004f6c
ngocphucdo <taken.jb@gmail.com>
2018-03-08 23:23:21 +0700

    commit

--- ss2/homework/models/customer.py
+++
@@ -1,9 +0,0 @@
-from mongoengine import Document, StringField, IntField, BooleanField
-
-class Customer(Document):
-    name = StringField()
-    gender = IntField()
-    email = StringField()
-    phone_number = StringField()
-    job = StringField()
-    contacted = BooleanField()
